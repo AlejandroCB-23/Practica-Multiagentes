020490289e87c06daddf823b1b70cb43dde018a3
Manuel Cano García <manuportero10@gmail.com>
2024-12-14 20:52:10 +0100

    feat: Create UI and logic structure for the AKINATOR web page

--- Proyecto/my-app/src/App.js
+++ Proyecto/my-app/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './LoginForm/LoginForm';
 import Dashboard from './Dashboard/Dashboard';
 import Akino from './akino/akino';
+import Cuestionario from './akino-cuestionario/akino-cuestionario';
+
 function App() {
     return (
         <Router>
@@ -9,6 +11,7 @@ function App() {
                 <Route path="/" element={<LoginForm />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/akino" element={<Akino />} />
+                <Route path="/akino-cuestionario" element={<Cuestionario />} />
             </Routes>
         </Router>
     );
